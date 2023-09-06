6845b53c7ade983ec741716c81e0f50c20e9cc5f
BadKarmaL33t <jchagesteijn@protonmail.com>
2023-09-06 21:07:20 +0200

    huiswerkopdrachten afronden

--- src/helpers/ListAllTelevisions.jsx
+++ src/helpers/ListAllTelevisions.jsx
@@ -1,5 +1,6 @@
 import FormatScreenSizes from "./FormatScreenSizes.js";
 import FormatOptions from "./FormatOptions.jsx";
+import out_of_stock from "../assets/out_of_stock.png";
 
 function listAllTelevisions(inventory) {
     return (
@@ -8,8 +9,12 @@ function listAllTelevisions(inventory) {
                 <li key={index}>
                     <article className="product product-standard-format">
         <span className="product-image">
-          <img src={tv.sourceImg} alt={`Afbeelding van ${tv.name}`}/>
-        </span>
+            {tv.originalStock - tv.sold > 0 ?
+                <img src={tv.sourceImg} alt={`Afbeelding van ${tv.name}`}/>
+                :
+                <img src={out_of_stock} alt="Out of stock image"/>
+            }
+                </span>
                         <div className="product-info">
                             <h4>{tv.brand} {tv.type} - {tv.name}</h4>
                             <h3>€{tv.price},-</h3>
