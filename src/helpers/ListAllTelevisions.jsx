2651ef13e93851cd4fa989add749d2ba4888a5ef
BadKarmaL33t <jchagesteijn@protonmail.com>
2023-09-06 20:07:49 +0200

    huiswerk les 2 opdracht 2 uitwerken

--- src/helpers/ListAllTelevisions.jsx
+++ src/helpers/ListAllTelevisions.jsx
@@ -1,9 +1,22 @@
-function listAllTelevisions(tv) {
+import FormatScreenSizes from "./FormatScreenSizes.js";
+import FormatOptions from "./FormatOptions.jsx";
+
+function listAllTelevisions(inventory) {
     return (
-        <ul className="brands-list">
-            {tv.map((tv, index) => (
+        <ul className="all-televisions-section">
+            {inventory.map((tv, index) => (
                 <li key={index}>
-                    {tv.brand}
+                    <article className="product product-standard-format">
+        <span className="product-image">
+          <img src={tv.sourceImg} alt={`Afbeelding van ${tv.name}`}/>
+        </span>
+                        <div className="product-info">
+                            <h4>{tv.brand} {tv.type} - {tv.name}</h4>
+                            <h3>€{tv.price},-</h3>
+                            <small>{FormatScreenSizes(tv)}</small>
+                            <small>{FormatOptions(tv)}</small>
+                        </div>
+                    </article>
                 </li>
             ))}
         </ul>
